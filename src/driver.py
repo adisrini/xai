6b4c1ab1c567d536b669ef79a1e058ef642e95a3
Aditya Srinivasan <asrinivasan@cinchapi.com>
2017-02-08 00:53:55 -0500

    Implement explanation initialization in model and ported linear programming to model

--- src/driver.py
+++ src/driver.py
@@ -3,46 +3,11 @@ import matplotlib.pyplot as plt
 from scipy.optimize import linprog
 from x_sgdc import ExplainableSGDClassifier
 
-EPSILON = 0.001
-
 if __name__ == '__main__':
     X = np.array([[-1, -1], [0, 0], [1, 1], [2, 2]])
     Y = np.array([-1, -1, 1, 1])
     model = ExplainableSGDClassifier()
     model.fit(X, Y)
-    coefs = model.reg.coef_[0]
-    b1 = coefs[0]
-    b2 = coefs[1]
-    b0 = model.reg.intercept_[0]
-    n = X[0].size
-    
-    print b0
-    print b1
-    print b2
-    
-    observation = [[0, 0]]
-    
-    label = model.predict(observation)[0]
-    
-    c = [0 for _ in range(n)] + [1 for _ in range(n)]
-        
-    A_ineq = []
-    for i in range(n):
-        row1 = [0 for _ in range(2*n)]; row1[i] = -1; row1[n + i] = -1
-        row2 = [0 for _ in range(2*n)]; row2[i] = 1; row2[n + i] = -1
-        A_ineq.append(row1)
-        A_ineq.append(row2)
-    
-    A_ineq.append([label*coefs[i] for i in range(n)] + [0 for i in range(n)])
-    
-    b_ineq = [f(x) for x in observation[0] for f in (lambda x: -x, lambda x: x)] + [label * -model.reg.intercept_[0] + label * EPSILON]
-    
-    bnds = ()
-    for i in range(2*n):
-        bnds = ((None, None),) + bnds
-    
-    res = linprog(c, A_ub = A_ineq, b_ub = b_ineq, bounds = bnds, options={"disp": True})
-    print res
-    
-    print model.predict(observation)
-    print model.predict([[res.x[0], res.x[1]]])
+    obs = [[-1000, -1000]]
+    explanation = model.explain(obs)
+    print explanation.top_k(2)
--- src/model.py
+++ src/model.py
@@ -1,4 +1,7 @@
 import operator
+from scipy.optimize import linprog
+
+EPSILON = 0.001
 
 class Explanation:
     
@@ -20,25 +23,25 @@ class ExplainableModel:
         """
         Trains the model with the provided training data.
         """
-        raise NotImplementedError()
-    
+        pass
+        
     def predict(self, X):
         """
         Predicts the output given the trained model and an observation.
         """
-        raise NotImplementedError()
+        pass
     
     def score(self, X, y):
         """
         Returns the coefficient of determination of the model.
         """
-        raise NotImplementedError()
-    
+        pass
+        
     def explain(self, X):
         """
         Returns an explanation given the trained model and an observation.
         """
-        raise NotImplementedError()
+        pass
     
 class Explainer:
     
@@ -46,4 +49,38 @@ class Explainer:
         """
         Returns an Explanation given a model and an observation
         """
-        raise NotImplementedError()
+        assert len(X) == 1
+        assert len(model.coef_) == 1
+        n = len(X[0])
+        coefs = model.coef_[0]
+        intercept = model.intercept_[0]
+        assert len(coefs) == n
+        label = model.predict(X)[0]
+        
+        c = [0 for _ in range(n)] + [1 for _ in range(n)]
+            
+        A_ineq = []
+        for i in range(n):
+            row1 = [0 for _ in range(2*n)]; row1[i] = -1; row1[n + i] = -1
+            row2 = [0 for _ in range(2*n)]; row2[i] = 1; row2[n + i] = -1
+            A_ineq.append(row1)
+            A_ineq.append(row2)
+        
+        A_ineq.append([label*coefs[i] for i in range(n)] + [0 for i in range(n)])
+        
+        b_ineq = [f(x) for x in X[0] for f in (lambda x: -x, lambda x: x)] + [label * -intercept + label * EPSILON]
+        
+        bnds = ()
+        for i in range(2*n):
+            bnds = ((None, None),) + bnds
+        
+        res = linprog(c, A_ub = A_ineq, b_ub = b_ineq, bounds = bnds, options={"disp": True})
+        print res
+        
+        print model.predict(X)
+        print model.predict([[res.x[0], res.x[1]]])
+        
+        shifts = {}
+        for i in range(n):
+            shifts["feature " + str(i)] = abs(X[0][i] - res.x[i])
+        return Explanation(shifts)
