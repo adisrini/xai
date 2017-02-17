773a5b619f3761742a4752dc214f70f38619cd9a
Aditya Srinivasan <asrinivasan@cinchapi.com>
2017-02-17 11:39:23 -0500

    Implement explainable perceptron

--- src/driver.py
+++ src/driver.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.optimize import linprog
 from x_linearsgdc import ExplainableSGDClassifier
 from x_linearsvc import ExplainableSVC
+from x_perceptron import ExplainablePerceptron
 
 if __name__ == '__main__':
     X = np.array([[-2, -200], [0, 0], [1, 100], [2, 200]])
     Y = np.array([-1, -1, 1, 1])
-    model = ExplainableSVC()
+    model = ExplainablePerceptron()
     model.fit(X, Y)
     obs = [[-1000, -1000]]
     explanation = model.explain(obs)
