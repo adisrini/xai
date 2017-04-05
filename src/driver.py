Remove range scaling for linear program (standardization will take care of that) and fix bug with epsilon relaxation

src/driver.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.optimize import linprog
-from models.x_linearsgdc import ExplainableSGDClassifier #ExplainableSGDClassifier, ExplainableLinearSVC, ExplainablePerceptron, ExplainableSVC
-
+from models.x_linearsgdc import ExplainableSGDClassifier
+from models.x_linearsvc import ExplainableLinearSVC
+from models.x_perceptron import ExplainablePerceptron
 
 if __name__ == '__main__':
     X = [[1, 10], [2, 20], [3, 30], [4, 40]]
     Y = [-1, -1, 1, 1]
-    obs = [[3, 35]]
-    model = ExplainableSGDClassifier()
+    obs = [[1, 15]]
+    model = ExplainableLinearSVC()
     model.fit(X, Y)     
-    print "predicted: ", model.predict(obs)[0]
-    print "score:     ", model.score(X, Y)
+    print "predicted: ", model.predict(obs)
+#     print "score:     ", model.score(X, Y)
     explanation = model.explain(obs)
     print explanation.top_k(5)
     print explanation.confidence()