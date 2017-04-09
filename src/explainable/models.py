23b3f06fb183c21a3d0abd72014bef1760b0efdf
Aditya Srinivasan <asrinivasan@cinchapi.com>
2017-04-09 14:11:45 -0400

    Setup database with modules and admin account

--- src/explainable/models.py
+++ src/explainable/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Module(models.Model):
+    module_title = models.CharField(max_length=200)
+    module_route = models.CharField(max_length=200)
+    module_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.module_title + ": " + self.module_description
--- src/explainable/views.py
+++ src/explainable/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world. You are at the explainable index.")
+    explainable_modules = {'flip': 'Quantify the robustness of a model when presented a new observation.'}
+    template = loader.get_template('explainable/index.html')
+    context = {
+        'options' : explainable_modules
+    }
+    return HttpResponse(template.render(context, request))
 
 def flip(request):
     return HttpResponse("You're looking at the flip module.")
