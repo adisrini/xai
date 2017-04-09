d68fa6d30ea70105cbfac9d8412582fe55fed6de
Aditya Srinivasan <asrinivasan@cinchapi.com>
2017-04-09 16:06:42 -0400

    Add more CSS styles and modify way modules are presented

--- src/explainable/models.py
+++ src/explainable/models.py
@@ -5,6 +5,7 @@ class Module(models.Model):
     module_title = models.CharField(max_length=200)
     module_route = models.CharField(max_length=200)
     module_description = models.CharField(max_length=200)
+    module_color = models.CharField(max_length=200)
 
     def __str__(self):
         return self.module_title + ": " + self.module_description
